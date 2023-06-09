a75010554d143596e5153ca95247322bef8d81de
YB Design <ybdesign01@gmail.com>
2023-06-09 09:26:30 +0100

    Added addCarburants route

--- src/main/java/com/app/estation/repository/HistoriquePrixRepository.java
+++ src/main/java/com/app/estation/repository/HistoriquePrixRepository.java
@@ -28,7 +28,7 @@ public interface HistoriquePrixRepository extends JpaRepository<HistoriquePrix, 
     List<Double> findPreviousPrixVente(@Param("idProduit") Long idProduit);
 
 
-    @Query("SELECT h FROM HistoriquePrix h WHERE h.idProduit = :idProduit AND h.dateDebut between :dateDebut AND :dateFin order by h.dateDebut ASC")
+    @Query("SELECT h FROM HistoriquePrix h WHERE h.idProduit = :idProduit AND h.dateDebut between :dateDebut AND :dateFin order by h.dateDebut desc")
     List<HistoriquePrix> findAllByIdProduitAndDateDebut(Produit idProduit, LocalDateTime dateDebut, LocalDateTime dateFin);
 
 
